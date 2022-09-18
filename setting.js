4f637024e8b13b8ca0b1d19f63214d9b19f3e2f7
kuh6969 <93518896+kuh6969@users.noreply.github.com>
2022-09-18 22:18:46 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.Api = {
+	ampibi: 'https://ampibismm.my.id',
+}
+
+// Apikey Website Api
+global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
+
+global.smm = {
+  nomor : '628989800046',
+  atasnama : 'Kukuh Ramadhan'
+}
+
+// Other
+global.owner = ['628989898299']
+global.premium = ['628989898299']
+global.sponsor = 'no sponsors in:('
+global.packname = 'KuhXD'
+global.footer = '© 2022 Kukuh R'
+global.author = 'IG : @ramadhankukuh'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.act = {
+		wait: 'please wait a moment, processing data...',
+		den:'*Sorry You Not Register Yet...*\n\n*.reg* <name>\n*To Regis/Login*',
+		proc:'Loading Please Wait..',
+		error:'Error :(',
+		success:'Success bang!',
+		repiar:'This Feature Maintace!',
+		botadm:'Jadiin gw admin dulu!',
+		admin:'Khusus admin woi!',
+		done:'Done!',
+		gc:'Only Group Allowed',
+		owner:'Only Owner Allowed'
+}
+
+global.thumb = fs.readFileSync('./lib/kuhlogo.jpeg')
+global.crizzy = fs.readFileSync('./lib/kuhlogo.jpeg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
